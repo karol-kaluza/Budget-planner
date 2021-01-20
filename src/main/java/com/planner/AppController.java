d09da9971be79f7cdda6683d9a7facbf9f5ce9db
vansmoe <61562397+vansmoe@users.noreply.github.com>
2021-01-20 12:18:06 +0100

    Connect expense.html to semantic ui

--- src/main/java/com/planner/AppController.java
+++ src/main/java/com/planner/AppController.java
@@ -1,11 +1,15 @@
 package com.planner;
 
 import com.planner.database.LocalDB;
+import com.planner.expense.Expense;
 import com.planner.expense.ExpenseService;
 import com.planner.user.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDate;
 
 @Controller
 public class AppController {
@@ -27,6 +31,14 @@ public class AppController {
         User user = new User("jacko12");
         user.setUserType("admin");
         model.addAttribute("user", user);
+        model.addAttribute("expense", new Expense());
         return "expense";
+    }
+
+    @PostMapping("addExpense")
+    public String add(@Valid Expense expense) {
+        expense.setDate(LocalDate.now());
+        System.out.println(expense.toString());
+        return "empty";
     }
 }
