3e17da51eceb3ecbe27c2a4e71897ddef95126b5
Karol Kałuża <karol.kaluza@gmail.com>
2021-02-17 14:55:56 +0100

    income form

--- src/main/java/com/planner/controller/MainController.java
+++ src/main/java/com/planner/controller/MainController.java
@@ -2,11 +2,13 @@ package com.planner.controller;
 
 import com.planner.cash_flow.ExpenseService;
 import com.planner.cash_flow.dto.ExpenseDto;
+import com.planner.cash_flow.dto.IncomeDto;
 import com.planner.cash_flow.model.Expense;
 import com.planner.currency.CurrencyRateProvider;
 import com.planner.database.LocalDB;
 import com.planner.repositories.ExpenseRepository;
 import com.planner.service.ServiceExp;
+import com.planner.service.ServiceInc;
 import com.planner.user.model.User;
 import com.planner.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
@@ -26,6 +28,7 @@ public class MainController {
 
     private CurrencyRateProvider currencyRateProvider;
     private ServiceExp expenseService;
+    private ServiceInc incomeServiece;
 
     @GetMapping("/main")
     public String index(Model model) {
@@ -33,8 +36,11 @@ public class MainController {
         model.addAttribute("expenses", expenseService.findAll());
         model.addAttribute("expenseDto", new ExpenseDto());
         model.addAttribute("currency", currencyRateProvider.getPrettyRate(CurrencyRateProvider.Currency.EUR));
+        model.addAttribute("incomes", incomeServiece.findAll());
+        model.addAttribute("incomeDto", new IncomeDto());
         return "main";
     }
+
 
     @GetMapping("/goodbye")
     public String goodbye() {
